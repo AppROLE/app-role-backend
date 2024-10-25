e78e36018051cfa6afb7988b0330ea57e09f4b0e
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-24 23:14:00 -0300

    refactor: Update CreateEventViewModel constructor to include event ID parameter

--- src/modules/create_event/app/create_event_viewmodel.ts
+++ src/modules/create_event/app/create_event_viewmodel.ts
@@ -1,13 +1,16 @@
 export class CreateEventViewModel {
   message: string;
+  id: string;
 
-  constructor(message: string) {
+  constructor(message: string, id: string) {
     this.message = message;
+    this.id = id;
   }
 
   toJSON() {
     return {
       message: this.message,
+      id: this.id,
     };
   }
 }
