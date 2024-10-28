f2099c23264ee4a122932c27755baced30e607cb
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-28 00:00:48 -0300

    refactor: Update IInstituteRepository to include optional parameters in updateInstitute method

--- src/shared/domain/irepositories/institute_repository_interface.ts
+++ src/shared/domain/irepositories/institute_repository_interface.ts
@@ -1,4 +1,5 @@
 import { Institute } from "../entities/institute";
+import { INSTITUTE_TYPE } from "../enums/institute_type_enum";
 import { PARTNER_TYPE } from "../enums/partner_type_enum";
 
 export interface IInstituteRepository {
@@ -8,5 +9,14 @@ export interface IInstituteRepository {
   getInstituteById(instituteId: string): Promise<Institute>;
   deleteInstituteById(instituteId: string): Promise<void>;
   updateInstitutePhoto(name: string, institutePhoto: string): Promise<string>;
-  updateInstitute(institute: Institute): Promise<void>;
+  updateInstitute(
+    institute_id: string,
+    description?: string,
+    institute_type?: INSTITUTE_TYPE,
+    partner_type?: PARTNER_TYPE,
+    name?: string,
+    address?: string,
+    district_id?: string,
+    phone?: string
+  ): Promise<Institute>;
 }
