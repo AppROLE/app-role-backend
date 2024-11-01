ec409997c923150405b6f1b1f0e5df7aff881ca1
lucapinheiro <lucapgomes11@gmail.com>
2024-10-31 23:04:34 -0300

    Refactor: Improve error handling and logging in GetAllFavoriteInstitutesController

--- src/modules/get_all_favorites_institutes/app/get_all_favorites_institutes_controller.ts
+++ src/modules/get_all_favorites_institutes/app/get_all_favorites_institutes_controller.ts
@@ -4,21 +4,32 @@ import {
   NotFound,
   OK,
 } from "src/shared/helpers/external_interfaces/http_codes";
-import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
+import {
+  ForbiddenAction,
+  NoItemsFound,
+} from "src/shared/helpers/errors/usecase_errors";
 import { MissingParameters } from "src/shared/helpers/errors/controller_errors";
 import { GetAllFavoriteInstitutesUseCase } from "./get_all_favorites_institutes_usecase";
 import { GetAllFavoriteInstitutesViewModel } from "./get_all_favorites_institutes_viewmodel";
+import { UserAPIGatewayDTO } from "src/shared/infra/dto/user_api_gateway_dto";
 
 export class GetAllFavoriteInstitutesController {
   constructor(private readonly usecase: GetAllFavoriteInstitutesUseCase) {}
 
-  async handle(req: IRequest): Promise<any> {
+  async handle(
+    req: IRequest,
+    requesterUser: Record<string, any>
+  ): Promise<any> {
     try {
+      const parsedUserApiGateway =
+        UserAPIGatewayDTO.fromAPIGateway(requesterUser).getParsedData();
+      if (!parsedUserApiGateway) throw new ForbiddenAction("usuário");
+
       console.log(
         "Handling request in GetAllFavoriteInstitutesController with data:",
         req.data
       );
-      const { username } = req.data;
+      const username = parsedUserApiGateway.username;
 
       if (!username) {
         console.log("Missing username parameter in request.");
