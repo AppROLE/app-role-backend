8fcff034f1cccb2d437ed16ee4e23b2f9fdefa5b
lucapinheiro <lucapgomes11@gmail.com>
2024-10-31 21:39:45 -0300

    feat: Add GetProfileReturnType to handle profile data

---
+++ src/shared/domain/types/get_profile_return_type.ts
@@ -0,0 +1,15 @@
+export type GetProfileReturnType = {
+    userId: string
+    nickname: string
+    username: string
+    linkTiktok?: string
+    linkInstagram?: string
+    biography: string | undefined
+    profilePhoto: string | undefined
+    backgroundPhoto: string | undefined
+    privacy: string
+    following: number
+    followers: number
+    isFriend: boolean | undefined
+    isFollowing: boolean | undefined
+  }
