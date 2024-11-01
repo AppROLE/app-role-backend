2020ede0da49a28fb57098ceba81a37d9c5bea58
lucapinheiro <lucapgomes11@gmail.com>
2024-10-31 23:04:38 -0300

    Refactor: Improve error handling and logging in GetAllFavoriteInstitutesPresenter

--- src/modules/get_all_favorites_institutes/app/get_all_favorites_institutes_presenter.ts
+++ src/modules/get_all_favorites_institutes/app/get_all_favorites_institutes_presenter.ts
@@ -1,26 +1,33 @@
 import { Environments } from "src/shared/environments";
-import { LambdaHttpRequest, LambdaHttpResponse } from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
 import { GetAllFavoriteInstitutesController } from "./get_all_favorites_institutes_controller";
 import { GetAllFavoriteInstitutesUseCase } from "./get_all_favorites_institutes_usecase";
+import { getRequesterUser } from "src/shared/utils/get_requester_user";
 
 const userRepo = Environments.getUserRepo();
 const instituteRepo = Environments.getInstituteRepo();
 const usecase = new GetAllFavoriteInstitutesUseCase(userRepo, instituteRepo);
 const controller = new GetAllFavoriteInstitutesController(usecase);
 
-export async function getAllFavoriteInstitutesPresenter(event: Record<string, any>) {
-    const httpRequest = new LambdaHttpRequest(event);
-    const response = await controller.handle(httpRequest);
-    const httpResponse = new LambdaHttpResponse(
-        response?.body,
-        response?.statusCode,
-        response?.headers
-    );
+export async function getAllFavoriteInstitutesPresenter(
+  event: Record<string, any>
+) {
+  const httpRequest = new LambdaHttpRequest(event);
+  const requesterUser = getRequesterUser(event);
+  const response = await controller.handle(httpRequest, requesterUser);
+  const httpResponse = new LambdaHttpResponse(
+    response?.body,
+    response?.statusCode,
+    response?.headers
+  );
 
-    return httpResponse.toJSON();
+  return httpResponse.toJSON();
 }
 
 export async function lambda_handler(event: any, context: any) {
-    const response = await getAllFavoriteInstitutesPresenter(event);
-    return response;
+  const response = await getAllFavoriteInstitutesPresenter(event);
+  return response;
 }
