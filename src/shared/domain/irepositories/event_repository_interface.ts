80c9f3974d878f564c0fd0e1955cc1db2a93a0ba
lucapinheiro <lucapgomes11@gmail.com>
2024-11-03 16:31:03 -0300

    Add getAllEventsFromToday method to IEventRepository interface

--- src/shared/domain/irepositories/event_repository_interface.ts
+++ src/shared/domain/irepositories/event_repository_interface.ts
@@ -3,6 +3,7 @@ import { Event } from "../entities/event";
 export interface IEventRepository {
   createEvent(event: Event): Promise<string>;
   getAllEvents(): Promise<Event[]>;
+  getAllEventsFromToday(): Promise<Event[]>;
   getEventsByFilter(filter: any): Promise<Event[]>;
   getEventById(eventId: string): Promise<Event>;
   deleteEventById(eventId: string): Promise<void>;
