13ee25dd441ecc993b91ff89496864fd5a32ad18
gabrielmerola <gabrielmerola12@gmail.com>
2024-11-04 14:36:19 -0300

    Update IEventRepository to include isMyEvents parameter in getAllConfirmedEvents method

--- src/shared/domain/irepositories/event_repository_interface.ts
+++ src/shared/domain/irepositories/event_repository_interface.ts
@@ -20,7 +20,7 @@ export interface IEventRepository {
     photoUrl: string,
     username: string
   ): Promise<void>;
-  getAllConfirmedEvents(username: string): Promise<Event[]>;
+  getAllConfirmedEvents(username: string, isMyEvents: boolean): Promise<Event[]>;
   updateEvent(eventId: string, updatedFields: any): Promise<Event>;
   updateEventBanner(eventId: string, bannerUrl: string): Promise<void>;
 }
