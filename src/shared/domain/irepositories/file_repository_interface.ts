6470bc88291a471119c9fe2b03cb8560fe97b9ab
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-11-10 17:26:34 -0300

    Refactor IFileRepository interface to standardize method parameters for event photo uploads

--- src/shared/domain/irepositories/file_repository_interface.ts
+++ src/shared/domain/irepositories/file_repository_interface.ts
@@ -1,9 +1,27 @@
 export interface IFileRepository {
-  uploadEventPhoto(imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
-  uploadEventGalleryPhoto(eventId: string, eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
-  uploadEventBanner(eventId: string, eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
+  uploadEventPhoto(
+    eventId: string,
+    eventName: string,
+    imageNameKey: string,
+    eventPhoto: Buffer,
+    mimetype: string
+  ): Promise<void>;
+  uploadEventGalleryPhoto(
+    eventId: string,
+    eventName: string,
+    imageNameKey: string,
+    eventPhoto: Buffer,
+    mimetype: string
+  ): Promise<void>;
+  uploadEventBanner(
+    eventId: string,
+    eventName: string,
+    imageNameKey: string,
+    eventPhoto: Buffer,
+    mimetype: string
+  ): Promise<void>;
   deleteEventPhotoByEventId(eventId: string): Promise<void>;
   deleteGallery(eventId: string): Promise<void>;
   deleteInstitutePhoto(name: string): Promise<void>;
-  deleteEventBanner(eventId: string, eventName: string): Promise<void>
+  deleteEventBanner(eventId: string, eventName: string): Promise<void>;
 }
