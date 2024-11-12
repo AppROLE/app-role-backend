db9886389f5d293fdf8a1eeb3cc6cb27735da6cd
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-11-12 18:08:56 -0300

    Update S3 object key format in FileRepositoryS3 to include imageNameKey in the key path

--- src/shared/infra/external-services/file_repository_s3.ts
+++ src/shared/infra/external-services/file_repository_s3.ts
@@ -21,7 +21,7 @@ export class FileRepositoryS3 implements IFileRepository {
       console.log("s3BucketName: ", this.s3BucketName);
       const params: S3.PutObjectRequest = {
         Bucket: this.s3BucketName,
-        Key: `${eventId}+${eventName}`,
+        Key: `${eventId}+${eventName}/${imageNameKey}`,
         Body: eventPhoto,
         ContentType: mimetype,
       };
