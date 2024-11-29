a403141c8b7ec2a2c2dc0dcd0f267f08113c7e2c
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-11-28 23:37:37 -0300

    Refactor FileRepositoryS3 to improve error messages and streamline S3 object deletion for event galleries

--- src/shared/infra/external-services/file_repository_s3.ts
+++ src/shared/infra/external-services/file_repository_s3.ts
@@ -193,37 +193,39 @@ export class FileRepositoryS3 implements IFileRepository {
     try {
       const s3 = new S3();
       console.log("s3BucketName: ", this.s3BucketName);
-
+  
       if (!eventId) {
         throw new Error("Event ID não fornecido.");
       }
 
+      const galleryPrefix = `events/${eventId}/gallery/`;
+  
       const listParams: S3.ListObjectsV2Request = {
         Bucket: this.s3BucketName,
-        Prefix: `${eventId}+`,
+        Prefix: galleryPrefix,
       };
-
+  
       const listedObjects = await s3.listObjectsV2(listParams).promise();
-
+  
       if (!listedObjects.Contents || listedObjects.Contents.length === 0) {
-        throw new Error("Nenhum arquivo encontrado no bucket.");
+        throw new Error(`Nenhum arquivo encontrado na pasta ${galleryPrefix}.`);
       }
-
+  
       const deleteParams: S3.DeleteObjectsRequest = {
         Bucket: this.s3BucketName,
         Delete: {
           Objects: listedObjects.Contents.map((file) => ({ Key: file.Key! })),
         },
       };
-
+  
       await s3.deleteObjects(deleteParams).promise();
-      console.log(
-        `Pasta e arquivos da galeria do evento ${eventId} deletados com sucesso.`
-      );
+      console.log(`Pasta e arquivos da galeria do evento ${eventId} deletados com sucesso.`);
     } catch (error: any) {
+      console.error(`Erro ao deletar a pasta da galeria: ${error.message}`);
       throw new Error(`Erro ao deletar a pasta da galeria: ${error.message}`);
     }
   }
+  
 
   async deleteEventBanner(eventId: string): Promise<void> {
     try {
