8e43d3078a3baf46f8d29bd19bb807de7888601b
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2025-01-06 00:11:11 -0300

    fix again

--- src/shared/infra/dto/user_api_gateway_dto.ts
+++ src/shared/infra/dto/user_api_gateway_dto.ts
@@ -1,24 +1,37 @@
 export class UserAPIGatewayDTO {
-  userId: string
-  username: string
-  email: string
+  userId: string;
+  username: string;
+  email: string;
+  nickname: string;
 
-  constructor(userId: string, username: string, email: string) {
-    this.userId = userId
-    this.username = username
-    this.email = email
+  constructor(
+    userId: string,
+    username: string,
+    email: string,
+    nickname: string
+  ) {
+    this.userId = userId;
+    this.username = username;
+    this.email = email;
+    this.nickname = nickname;
   }
 
   static fromAPIGateway(data: Record<string, any>): UserAPIGatewayDTO {
-    console.log('data FROM API GATEWAY ', data)
-    return new UserAPIGatewayDTO(data['sub'], data['cognito:username'], data['email'])
+    console.log("data FROM API GATEWAY ", data);
+    return new UserAPIGatewayDTO(
+      data["sub"],
+      data["cognito:username"],
+      data["email"],
+      data["nickname"] || ""
+    );
   }
 
   getParsedData() {
     return {
       userId: this.userId,
       username: this.username,
-      email: this.email
-    }
+      email: this.email,
+      nickname: this.nickname,
+    };
   }
 }
