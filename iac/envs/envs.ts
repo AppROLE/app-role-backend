b1277283d62dfddc0ac2ce568d30470edbbff67e
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2025-01-06 00:42:00 -0300

    Update envs.ts

--- iac/envs/envs.ts
+++ iac/envs/envs.ts
@@ -1,17 +1,15 @@
-/// <reference path="./envs.d.ts" />
-
 import { config } from "dotenv";
 import path from "path";
 
 config({ path: path.resolve(__dirname, "../../.env") });
 
 export const envs = {
-  STAGE: process.env.STAGE || "TEST",
-  AWS_REGION: process.env.AWS_REGION || "sa-east-1",
-  APP_CLIENT_ID: process.env.APP_CLIENT_ID || "",
-  USER_POOL_ID: process.env.USER_POOL_ID || "",
-  USER_POOL_ARN: process.env.USER_POOL_ARN || "",
-  MONGO_URI: process.env.MONGO_URI || "",
-  BUCKET_NAME: process.env.BUCKET_NAME || "",
-  STACK_NAME: process.env.STACK_NAME || "AppRoleStack",
+  STAGE: process.env.STAGE!,
+  AWS_REGION: process.env.AWS_REGION!,
+  APP_CLIENT_ID: process.env.APP_CLIENT_ID!,
+  USER_POOL_ID: process.env.USER_POOL_ID!,
+  USER_POOL_ARN: process.env.USER_POOL_ARN!,
+  MONGO_URI: process.env.MONGO_URI!,
+  BUCKET_NAME: process.env.BUCKET_NAME!,
+  STACK_NAME: process.env.STACK_NAME!,
 };
