9291b3233eb15b99d91f28182fd4accfbe121e26
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2025-01-10 00:30:38 -0300

    Update get_top_events_usecase.ts

--- src/modules/get_top_events/app/get_top_events_usecase.ts
+++ src/modules/get_top_events/app/get_top_events_usecase.ts
@@ -1,8 +1,8 @@
-import { Event } from "src/shared/domain/entities/event";
-import { IEventRepository } from "src/shared/domain/repositories/event_repository_interface";
-import { IPresenceRepository } from "src/shared/domain/repositories/presence_repository_interface";
-import { Repository } from "src/shared/infra/database/repositories/repository";
-import { getUpcomingWeekdays } from "src/shared/utils/date_utils";
+import { Event } from 'src/shared/domain/entities/event';
+import { IEventRepository } from 'src/shared/domain/repositories/event_repository_interface';
+import { IPresenceRepository } from 'src/shared/domain/repositories/presence_repository_interface';
+import { Repository } from 'src/shared/infra/database/repositories/repository';
+import { getUpcomingWeekdays } from 'src/shared/helpers/utils/date_utils';
 
 export class GetTopEventsUseCase {
   repository: Repository;
@@ -22,10 +22,10 @@ export class GetTopEventsUseCase {
     this.presence_repo = this.repository.presence_repo;
 
     if (!this.event_repo)
-      throw new Error("Expected to have an instance of the event repository");
+      throw new Error('Expected to have an instance of the event repository');
     if (!this.presence_repo)
       throw new Error(
-        "Expected to have an instance of the presence repository"
+        'Expected to have an instance of the presence repository'
       );
   }
 
