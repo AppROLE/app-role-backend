f05da94ba9dc95a119fa8f0aaccce7dc4266a45d
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2025-01-11 14:45:32 -0300

    update delete event

--- src/modules/delete_event/app/delete_event_usecase.ts
+++ src/modules/delete_event/app/delete_event_usecase.ts
@@ -1,5 +1,6 @@
 import { IEventRepository } from 'src/shared/domain/repositories/event_repository_interface';
 import { IFileRepository } from 'src/shared/domain/repositories/file_repository_interface';
+import { IInstituteRepository } from 'src/shared/domain/repositories/institute_repository_interface';
 import { NoItemsFound } from 'src/shared/helpers/errors/errors';
 import { Repository } from 'src/shared/infra/database/repositories/repository';
 
@@ -7,11 +8,13 @@ export class DeleteEventUsecase {
   repository: Repository;
   private event_repo?: IEventRepository;
   private file_repo?: IFileRepository;
+  private institute_repo?: IInstituteRepository;
 
   constructor() {
     this.repository = new Repository({
       event_repo: true,
       file_repo: true,
+      institute_repo: true,
     });
   }
 
@@ -19,6 +22,7 @@ export class DeleteEventUsecase {
     await this.repository.connectRepository();
     this.event_repo = this.repository.event_repo;
     this.file_repo = this.repository.file_repo;
+    this.institute_repo = this.repository.institute_repo;
 
     if (!this.event_repo)
       throw new Error('Expected to have an instance of the event repository');
@@ -34,5 +38,10 @@ export class DeleteEventUsecase {
 
     await this.event_repo!.deleteEvent(eventId);
     await this.file_repo!.deleteFolder(`events/${eventId}`);
+
+    const institute = await this.institute_repo!.getInstituteById(event.instituteId!);
+    if (!institute) return
+
+    await this.institute_repo!.updateInstitute(event.instituteId!, { eventsId: institute.eventsId!.filter((id) => id !== eventId) });
   }
 }
