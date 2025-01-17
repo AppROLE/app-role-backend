b6e17350a034949b5ac2de9763393d4cc06c0e5a
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2025-01-17 03:08:58 -0300

    Update delete_user_usecase.ts

--- src/modules/delete_user/delete_user_usecase.ts
+++ src/modules/delete_user/delete_user_usecase.ts
@@ -1,5 +1,6 @@
 import { IAuthRepository } from 'src/shared/domain/repositories/auth_repository_interface';
 import { IEmailRepository } from 'src/shared/domain/repositories/email_interface';
+import { IFileRepository } from 'src/shared/domain/repositories/file_repository_interface';
 import { IProfileRepository } from 'src/shared/domain/repositories/profile_repository_interface';
 import { NoItemsFound } from 'src/shared/helpers/errors/errors';
 import { accountDeletionEmailBody } from 'src/shared/helpers/utils/delete_user_email_body';
@@ -10,12 +11,14 @@ export class DeleteUserUsecase {
   private auth_repo?: IAuthRepository;
   private profile_repo?: IProfileRepository;
   private email_repo?: IEmailRepository;
+  private file_repo?: IFileRepository;
 
   constructor() {
     this.repository = new Repository({
       auth_repo: true,
       profile_repo: true,
       email_repo: true,
+      file_repo: true,
     });
   }
 
@@ -24,6 +27,7 @@ export class DeleteUserUsecase {
     this.auth_repo = this.repository.auth_repo;
     this.profile_repo = this.repository.profile_repo;
     this.email_repo = this.repository.email_repo;
+    this.file_repo = this.repository.file_repo;
 
     if (!this.auth_repo)
       throw new Error('Expected to have an instance of the auth repository');
@@ -33,6 +37,9 @@ export class DeleteUserUsecase {
 
     if (!this.email_repo)
       throw new Error('Expected to have an instance of the email repository');
+
+    if (!this.file_repo)
+      throw new Error('Expected to have an instance of the file repository');
   }
 
   async execute(userId: string) {
@@ -42,6 +49,8 @@ export class DeleteUserUsecase {
     await this.profile_repo!.deleteProfile(userId);
 
     await this.auth_repo!.deleteUser(profile.email);
+
+    await this.file_repo!.deleteFolder(`profiles/${userId}`);
 
     await this.email_repo!.sendEmail({
       to: profile.email,
